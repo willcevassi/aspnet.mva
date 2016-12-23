02968e0b891eb9a4c3840eeea07ef7921876d87e
Willian Cevassi <willian.cevassi@outlook.com.br>
2016-12-23 12:30:41 -0200

    Criação do CRUD Livros

---
+++ asp.mvc5.basico.web/Controllers/LivrosController.cs
@@ -0,0 +1,123 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using asp.mvc5.basico.web.Models;
+
+namespace asp.mvc5.basico.web.Controllers
+{
+    public class LivrosController : Controller
+    {
+        private BancoContext db = new BancoContext();
+
+        // GET: Livros
+        public ActionResult Index()
+        {
+            return View(db.Livros.ToList());
+        }
+
+        // GET: Livros/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Livro livro = db.Livros.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(livro);
+        }
+
+        // GET: Livros/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Livros/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Titulo,Autor,Valor,Estoque")] Livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Livros.Add(livro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(livro);
+        }
+
+        // GET: Livros/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Livro livro = db.Livros.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(livro);
+        }
+
+        // POST: Livros/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Titulo,Autor,Valor,Estoque")] Livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(livro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(livro);
+        }
+
+        // GET: Livros/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Livro livro = db.Livros.Find(id);
+            if (livro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(livro);
+        }
+
+        // POST: Livros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Livro livro = db.Livros.Find(id);
+            db.Livros.Remove(livro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ asp.mvc5.basico.web/Models/Livro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asp.mvc5.basico.web.Models
+{
+    public class Livro
+    {
+        public int ID { get; set; }
+
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public int Estoque { get; set; }
+    }
+}
